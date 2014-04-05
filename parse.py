2e4e00bab0c9bab2a13dbce1751c85fa0c336343
Ange BOUABLE <angebouabre@gmail.com>
2014-04-06 00:01:57 +0200

    Update the parse function

--- parse.py
+++ parse.py
@@ -3,41 +3,51 @@ import os
 import shutil
     
     
-dossier = '/home/bouable/workspace/project/sfr/neufbox-evol/integration/testi/fusion/13.2.20/2014/4/3/'
+dossier = '/home/bouable/sfr-workspace/fusion/13.2.20/2014/3/31'
 
 pk_scenario = 1
 pk_theme = 1
 pk_test = 1
 pk_campagne = 15 
-pk_mesure = 1  
+pk_mesure = 2003  
+
+tab_campagne = []
+tab_scenario = []
+tab_theme = []
+tab_test = []
+tab_mesure = []
 
 for fic in os.listdir(dossier):
     fic = os.path.join(dossier,fic)
     f = WitbeLogFile(fic)
-    f.serialize_scenario(pk_scenario)
-    f.serialize_theme(pk_theme)
-    f.serialize_test(pk_test)
-    f.serialize_mesure(pk_mesure)
-    
-fixture = "/home/bouable/workspace/project/sfr/neufbox-evol/integration/testi/guiwi-parser/fixture"
-
-for fic_gen in os.listdir(fixture):
-    
-    if "scenario" in fic_gen:
+    res_campagne = f.serialize_campagne(pk_campagne)
+    if res_campagne == True:
+        pk_campagne += 1
+        tab_campagne.append(pk_campagne)
+    res_scenario = f.serialize_scenario(pk_scenario)
+    if res_scenario == True:
         pk_scenario += 1
-    if "theme" in fic_gen:
+        tab_scenario.append(pk_scenario)
+    res_theme = f.serialize_theme(pk_theme)
+    if res_theme == True:
         pk_theme += 1
-    if "test" in fic_gen:
+        tab_theme.append(pk_theme)
+    res_test = f.serialize_test(pk_test)
+    if res_test == True:
         pk_test += 1
-    if "mesure" in fic_gen:
-        pk_mesure += 1
+        tab_test.append(pk_test)
+    res_mesure = f.serialize_mesure(pk_mesure)
+    if res_mesure == True:
+        pk_mesure += 1 
+        tab_mesure.append(pk_mesure)
 
-shutill.rmtree()
-os.makedirs(fixture)
-os.makedirs(fixture+'/principal')
+print tab_campagne
+print tab_scenario
+print tab_theme
+print tab_test
+print tab_mesure
 
-print pk_campagne
-print pk_theme
-print pk_scenario
-print pk_test
-print pk_mesure
+
+#shutill.rmtree()
+#os.makedirs(fixture)
+#os.makedirs(fixture+'/principal')
